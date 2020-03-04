5f44e35e06c209d02768c2659f819ee7ae4d60fe
Nat! <nat-tv@mulle-kybernetik.com>
2020-03-04 13:27:48 +0100

    progress + some debugging code

--- src/UIWindow.m
+++ src/UIWindow.m
@@ -344,7 +344,7 @@ static void   error_callback(int code, const char* description)
          [self updateRenderCachesWithContext:context 
                                    frameInfo:&info];
 
-         info.isPerfEnabled = YES;
+//         info.isPerfEnabled = YES;
          [context startRenderWithFrameInfo:&info];
          [self renderWithContext:context];
          [context endRender];
